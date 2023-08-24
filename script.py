8530c895ce745642a9395b3582c2786e0969e9dd
Kirill <108629431+moznoporusski@users.noreply.github.com>
2023-08-24 10:07:13 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,32 +1,21 @@
-
-# Заменить интернет интерфейс
-# Заменить айпи для каронты
-
-
 import os
 import time
 import subprocess
-
-a = 1
+from datetime import datetime
 
 while True:
-    # Запуск tcpdump
-    subprocess.Popen(['sudo','tcpdump', '-ni', 'eth1', 'port', '80', '-w', f'traffic{a}.pcap', '-v'])
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    pcap_filename = f'traffic_{timestamp}.pcap'
     
-    # Ожидание 30 секунд
+    tcpdump_process = subprocess.Popen(['sudo', 'tcpdump', '-ni', 'eth1', 'port', '80', '-w', pcap_filename, '-v'])
+    
     time.sleep(30)
     
-    # Завершение tcpdump
-    subprocess.run(['pkill', 'tcpdump'])
+    tcpdump_process.terminate()
     
-    # Выполнение curl
     subprocess.run(['curl', '-X', 'POST', 'http://10.60.30.129:3333/api/pcap/upload',
                     '-H', 'Content-Type: multipart/form-data',
-                    '-F', f'file=@traffic{a}.pcap',
+                    '-F', f'file=@{pcap_filename}',
                     '-F', 'flush_all=true'])
     
-    # Удаление файла
-    os.remove(f'traffic{a}.pcap')
-    
-    # Увеличение значения a
-    a += 1
+    os.remove(pcap_filename)
