77dce91770fed48cd24b82972050db03aa72b1b8
Kirill <108629431+moznoporusski@users.noreply.github.com>
2023-11-03 22:42:46 +0300

    ПОМЕНЯТЬ ИНТЕРФЕЙС

--- script.py
+++ script.py
@@ -3,19 +3,20 @@ import time
 import subprocess
 from datetime import datetime
 
+timestamp = 0
+
 while True:
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     pcap_filename = f'traffic_{timestamp}.pcap'
     
-    tcpdump_process = subprocess.Popen(['sudo', 'tcpdump', '-ni', 'eth1', 'port', '80', '-w', pcap_filename, '-v'])
+    tcpdump_process = subprocess.Popen(['sudo', 'tcpdump', '-i', 'enp2s0', '-w', pcap_filename, '-v'])
     
     time.sleep(30)
     
-    tcpdump_process.terminate()
-    
-    subprocess.run(['curl', '-X', 'POST', 'http://10.60.30.129:3333/api/pcap/upload',
+    tcpdump_process.kill()
+    subprocess.run(['curl', '-X', 'POST', 'http://moznoporusski.ru:3333/api/pcap/upload',
                     '-H', 'Content-Type: multipart/form-data',
                     '-F', f'file=@{pcap_filename}',
                     '-F', 'flush_all=true'])
     
     os.remove(pcap_filename)
+    timestamp += 1
