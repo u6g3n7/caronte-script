dca5a41f8ee73f249f1a3dce332a4af7629fbef5
Kirill <108629431+u6g3n7@users.noreply.github.com>
2024-10-17 22:12:17 +0300

    Update script.py

--- script.py
+++ script.py
@@ -16,7 +16,7 @@ keyboard.add_hotkey('ctrl+shift+x', stop_script)
 while should_continue:
     pcap_filename = f'traffic_{timestamp}.pcap'
     
-    tcpdump_process = subprocess.Popen(['sudo', 'tcpdump', '-i', 'wlan0', '-w', pcap_filename, '-v']) # change ethetnet
+    tcpdump_process = subprocess.Popen(['sudo', 'tcpdump', '-i', 'ens33', '-w', pcap_filename, '-v']) # change ethetnet
     
 
     for _ in range(15):
@@ -28,7 +28,7 @@ while should_continue:
     tcpdump_process.wait()
     
     if should_continue:
-        subprocess.run(['curl', '-X', 'POST', 'http://localhost:3333/api/pcap/upload', # change ip addres
+        subprocess.run(['curl', '-X', 'POST', 'http://10.8.0.2:3333/api/pcap/upload', # change ip addres
                         '-H', 'Content-Type: multipart/form-data',
                         '-F', f'file=@{pcap_filename}',
                         '-F', 'flush_all=true'])
